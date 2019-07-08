f25791aa03d7dece74eb82a9bb70540c2b13c911
SofiaFredriksson <sofiapkf@gmail.com>
2019-07-08 11:04:33 +0100

    reseed, added staff and categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,17 @@ Solution.destroy_all
 Category.destroy_all
 
 c1 = Category.create(name: "Miscellaneous")
+c2 = Category.create(name: "JavaScript")
+
+c3 = Category.create(name: "Ruby")
+c4 = Category.create(name: "Rails")
+c5 = Category.create(name: "React")
+
 
 s1 = Staff.create(name: "Sofia Fredriksson", title: "Software Engineering Coach", username: "FiaPia", password: "fia")
+s2 = Staff.create(name: "Sarah Jacob", title: "Software Engineering Coach", username: "sJacod", password: "fia")
+s3 = Staff.create(name: "Stuart Sewell", title: "Software Engineering Coach", username: "StewBrew", password: "fia")
+
 
 p1 = Post.create(title: "Heckin Concern", content: "Doin me a heck!", category: c1, staff: s1)
 
