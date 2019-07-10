a72da20a9970f4b8e7319c670fd4f70b994a7829
SofiaFredriksson <sofiapkf@gmail.com>
2019-07-10 16:19:30 +0100

    nav styling

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -17,7 +17,7 @@ class AccessController < ApplicationController
     if authorized_user
       session[:user_id] = authorized_user.id
       flash[:notice] = "You are now logged in."
-      redirect_to(admin_path)
+      redirect_to(posts_path)
     else
       flash.now[:notice] = "Invalid usernam/password combination."
       render('login')
